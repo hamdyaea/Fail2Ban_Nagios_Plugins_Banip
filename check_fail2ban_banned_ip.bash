166b7e305a7515bd0066b0aed8d8733c55090cd2
Abou El Anein <Hamdy.AbouElAnein@kantar.com>
2019-12-09 15:01:42 +0100

    improve check

--- check_fail2ban_banned_ip.bash
+++ check_fail2ban_banned_ip.bash
@@ -4,15 +4,17 @@
 # hamdy.aea@protonmail.com
 #
 ########################################################################################
-# This Nagios plugins is developed by : Hamdy Abou El Anein hamdy.aea@protonmail.com        #
+# Nagios plugins is developed by : Hamdy Abou El Anein hamdy.aea@protonmail.com        #
 # It check if fail2ban have an active ip banned and if yes it show the ip              #
 # In the script I use warning state only critical                                      #
-# Version 1.1                                                                          #
+# Version 2.0                                                                          # 
 ########################################################################################
 #
 # Variables declaration
 result=$(fail2ban-client status sshd | grep "Currently banned:" | cut -d : -f 2)
 bannedip=$(fail2ban-client status sshd | grep "Banned IP list:" | cut -d : -f 2)
+result2=$(fail2ban-client status sshd | grep "Currently banned:" | cut -d : -f 2| tr -s " ")
+performance="'BannedIP'=${result2//[[:blank:]]/};70;100"
 # Function OK
 function f_ok {
     echo "OK: "${1}
@@ -23,11 +25,6 @@ function f_cri {
     echo "CRITICAL: "${1}
     exit 2
 }
-# Function warning
-function f_war {
-    echo "WARNING:"${1}
-    exit 1
-}
 # Function unknown
 function f_unk {
     echo "UNKNOWN: "${1}
@@ -35,17 +32,17 @@ function f_unk {
 }
 # Function fail2banipbanned
 function fail2banipbanned {
-    # Check if an ip adress is banned or no.
+# Check if an ip adress is banned or no.
     if [ ${result} = 1 ]; then
-        f_cri "The ip $bannedip is banned"
+        f_cri "The ip $bannedip is banned | $performance"
     fi
     if [ ${result} -gt 1 ]; then
-        f_cri "The ip $bannedip are banned"
+        f_cri "The ip $bannedip are banned | $performance"
     fi
     if [[ ${result} =~ ^-?[0-9]+$ ]] ; then
-        f_unk "The list of banned ip is unknown"
+        f_unk "The list of banned ip is unknown | $performance"
     else
-        f_ok "No ip adress is banned"
+        f_ok "No ip adress is banned | $performance"
     fi
 }
 fail2banipbanned
